2ddc1c889b130e514e06619787e51ad32a0ff5c4
TakuyaKawabe <rapidarrow2000@gmail.com>
2019-02-15 11:43:45 +0900

    ファイル操作用Utliクラス追加

---
+++ SimilarImagesSerachTool/SimilarImagesSerachTool/Utli/File.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimilarImagesSearchTool.Utli
+{
+    public class File
+    {
+        public static FileInfo GetFileInfo(string path)
+        {
+            if (string.IsNullOrWhiteSpace(path))
+                throw new ArgumentNullException(nameof(path));
+
+
+            return  new System.IO.FileInfo(path);
+
+        }
+    }
+}
